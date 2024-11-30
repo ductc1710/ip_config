40c4640e6147d54ca9cfeff66b4bc2262c40f9f8
Trần Công Đức <tranduc17101999@gmail.com>
2024-12-01 04:44:53 +0700

    commit

--- bin/ip_config.dart
+++ bin/ip_config.dart
@@ -5,7 +5,7 @@ import 'package:ip_config/ip_config.dart' as ip_config;
 void main(List<String> arguments) async {
   final file = File('ip.txt');
   final ip = await file.readAsString();
-  final newContent = 'Hoa quả:$ip:14444:0:0,Localhost:127.0.0.1:14441:0:0,';
+  final newContent = 'Hoa quả:$ip:14444:0:0,Localhost:127.0.0.1:14441:0:0';
 
   file.writeAsString(newContent);
 }

3c9417bd581e485e77ad38780782ca93738291f6
Trần Đức <44558894+tranduc1710@users.noreply.github.com>
2025-08-03 14:51:13 +0700

    update ip

--- bin/ip_config.dart
+++ bin/ip_config.dart
@@ -4,7 +4,7 @@ import 'package:ip_config/ip_config.dart' as ip_config;
 
 void main(List<String> arguments) async {
   final file = File('ip.txt');
-  final ip = await file.readAsString();
+  final ip = (await file.readAsString()).trim();
   print('IP hien tai: $ip');
   final newContent = 'Hoa quả:$ip:14444:0:0,Localhost:127.0.0.1:14441:0:0';
 
